0d9bc8af49e339d33b749a14fb04d50def12e661
dinesh10123 <159063788+dinesh10123@users.noreply.github.com>
2024-08-17 22:33:55 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,19 @@
+package java_intro;
+
+
+
+public class Main{
+public static void main(String[] args){
+    //println is for sigle line assigne
+    System.out.println("Hello world");
+    System.out.print("Hello world\n");
+
+    //Pattern 
+    for(int i=1; i<5 ; i++){
+        for(int j=1; j<=i;j++){
+       System.out.print("*");
+    }
+ System.out.print("\n");
+}
+}
+}
